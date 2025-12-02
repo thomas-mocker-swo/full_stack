dde07d4bf433589b8b5a10ab95b109c4c6dade0d
Thomas.Mocker <thomas.mocker@softwareone.com>
2025-12-02 22:47:42 +0100

    test

--- sa.tf
+++ sa.tf
@@ -3,11 +3,11 @@ resource "azurerm_storage_account" "central" {
   account_kind             = "StorageV2"
   account_replication_type = "LRS"
   account_tier             = "Standard"
-  #checkov:skip=CKV_AZURE_43:The Name does comply with the required pattern and length and company naming conventions
-  #checkov:skip=CKV2_AZURE_38:OK
-  #checkov:skip=CKV2_AZURE_41:OK
-  #checkov:skip=CKV_AZURE_33:OK
-  #checkov:skip=CKV_AZURE_206:OK
+  #checkov:skip=CKV2_AZURE_38:Ensure soft-delete is enabled on Azure storage account
+  #checkov:skip=CKV2_AZURE_41:Ensure storage account is configured with SAS expiration policy
+  #checkov:skip=CKV_AZURE_33:Ensure Storage logging is enabled for Queue service for read, write and delete requests
+  #checkov:skip=CKV_AZURE_206:Ensure that Storage Accounts use replication
+  #checkov:skip=CKV2_AZURE_1:Ensure storage for critical data are encrypted with Customer Managed Key
   name                             = "euntmockerdemostorage"
   resource_group_name              = azurerm_resource_group.demo.name
   location                         = var.location

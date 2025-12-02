4498b51fda1c5278513de634992015e6205b8c51
Thomas.Mocker <thomas.mocker@softwareone.com>
2025-12-02 21:32:57 +0100

    demo

--- sa.tf
+++ sa.tf
@@ -4,7 +4,7 @@ resource "azurerm_storage_account" "central" {
   account_replication_type         = "LRS"
   account_tier                     = "Standard"
   name                             = "euntmockerdemostoragecentral"
-  resource_group_name              = azurerm_resource_group.identity.name
+  resource_group_name              = azurerm_resource_group.demo.name
   location                         = var.env.location
   https_traffic_only_enabled       = false
   allow_nested_items_to_be_public  = false

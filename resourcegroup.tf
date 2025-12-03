3e7a49991ab32443e29f0ce7fe5f8c1222c38d88
Thomas.Mocker <thomas.mocker@softwareone.com>
2025-12-03 08:44:03 +0100

    DEMO

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "demo" {
+resource "azurerm_resource_group" "DEMO" {
   name     = "${var.location_short}-demo-rg"
   location = var.location
   tags     = var.tags
